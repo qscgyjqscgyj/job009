74477e1764f9070f69641ce655dfd91184120905
mike <aliestarten@gmail.com>
2013-09-30 17:45:00 +0800

    add user models and correct deps.pip

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,59 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from captcha.fields import CaptchaField
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+from registration.models import RegistrationManager
 
-# Create your models here.
+
+#соискатель
+class CustomApplicant(User):
+    captcha = CaptchaField()
+
+    objects = RegistrationManager()
+
+    def __unicode__(self):
+        return self.username
+
+    class Meta:
+
+        def __init__(self):
+            pass
+
+        verbose_name = _(u'Соискатель')
+        verbose_name_plural = _(u'Соискатели')
+
+
+#работодатель
+class CustomEmployer(User):
+    captcha = CaptchaField()
+
+    objects = RegistrationManager()
+
+    def __unicode__(self):
+        return self.username
+
+    class Meta:
+
+        def __init__(self):
+            pass
+
+        verbose_name = _(u'Работодатель')
+        verbose_name_plural = _(u'Работодатели')
+
+
+#кадровое агентство
+class CustomAgency(User):
+    captcha = CaptchaField()
+
+    objects = RegistrationManager()
+
+    def __unicode__(self):
+        return self.username
+
+    class Meta:
+
+        def __init__(self):
+            pass
+
+        verbose_name = _(u'Кадровое агентство')
+        verbose_name_plural = _(u'Кадровые агентствая')
