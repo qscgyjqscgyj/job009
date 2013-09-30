7ef4f5aeb76d87ff3b435d0167a66a1beda2edf2
mike <aliestarten@gmail.com>
2013-09-30 15:34:49 +0800

    add local.py

---
+++ job009/local.py
@@ -0,0 +1,13 @@
+# Local settings template for job009 project.
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'job009',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'qscgyjjygcsq',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
