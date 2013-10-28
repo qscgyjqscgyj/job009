49266dfdcfb5526c93771bacd62d21b4bb2750b5
mike <aliestarten@gmail.com>
2013-10-28 17:49:16 +0800

    set lxml in view

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from jobs.models import Job
 from resume.models import Resume
 import urllib
-from bs4 import BeautifulSoup
+import lxml.html
 
 
 class MainView(ListView):
@@ -20,12 +20,16 @@ class MainView(ListView):
 
 def get_banners_info(page):
     page = urllib.urlopen("http://www.2-999-999.ru/")
-    soup = BeautifulSoup(page.read(), from_encoding="utf-8")
-    weather = soup.find(id='weather-banner')
-    weather_td = weather.findAll('td')
-    currency = soup.find(id='currency-banner')
-    currency_td = currency.findAll('td')
-    banners = {'weather_date': weather.th.text, 'weather_night': weather_td[1].text,
-               'weather_day': weather_td[3].text, 'currency_date': currency.th.text,
-               'currency_usd': currency_td[1].text, 'currency_eur': currency_td[3].text}
+    soup = lxml.html.document_fromstring(page.read())
+    weather = soup.get_element_by_id(id='weather-banner')
+    currency = soup.get_element_by_id(id='currency-banner')
+    weather_date = weather.find('table').findall('tr')[0].find('th').text
+    weather_night = weather.find('table').findall('tr')[1].findall('td')[1].text
+    weather_day = weather.find('table').findall('tr')[2].findall('td')[1].text
+    currency_date = currency.find('table').findall('tr')[0].find('th').text
+    currency_usd = currency.find('table').findall('tr')[1].findall('td')[1].text
+    currency_eur = currency.find('table').findall('tr')[2].findall('td')[1].text
+    banners = {'weather_date': weather_date, 'weather_night': weather_night,
+               'weather_day': weather_day, 'currency_date': currency_date,
+               'currency_usd': currency_usd, 'currency_eur': currency_eur}
     return {'banners': banners}
