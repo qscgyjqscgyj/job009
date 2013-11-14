37f1f8ce5246b2397fbbe7b13bb6d61496ee4ef1
mike <aliestarten@gmail.com>
2013-11-14 18:52:09 +0800

    add custom city model

---
+++ main/migrations/0007_auto__add_city__chg_field_adarea_city.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'City'
+        db.create_table(u'main_city', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal(u'main', ['City'])
+
+
+        # Changing field 'AdArea.city'
+        db.alter_column(u'main_adarea', 'city_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.City']))
+
+    def backwards(self, orm):
+        # Deleting model 'City'
+        db.delete_table(u'main_city')
+
+
+        # Changing field 'AdArea.city'
+        db.alter_column(u'main_adarea', 'city_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['django_geoip.City']))
+
+    models = {
+        u'main.adarea': {
+            'Meta': {'object_name': 'AdArea'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'areas'", 'to': u"orm['main.City']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.adcategory': {
+            'Meta': {'object_name': 'AdCategory'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.ademployment': {
+            'Meta': {'object_name': 'AdEmployment'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.adexperience': {
+            'Meta': {'object_name': 'AdExperience'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.adsalarymeasure': {
+            'Meta': {'object_name': 'AdSalaryMeasure'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.adschedule': {
+            'Meta': {'object_name': 'AdSchedule'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.adsubcategory': {
+            'Meta': {'object_name': 'AdSubCategory'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'ad_sub_category_category'", 'to': u"orm['main.AdCategory']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.adtime': {
+            'Meta': {'object_name': 'AdTime'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.city': {
+            'Meta': {'object_name': 'City'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'main.companycategory': {
+            'Meta': {'object_name': 'CompanyCategory'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'main.education': {
+            'Meta': {'object_name': 'Education'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'main.employees': {
+            'Meta': {'object_name': 'Employees'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'main.gender': {
+            'Meta': {'object_name': 'Gender'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'main.maritalstatus': {
+            'Meta': {'object_name': 'MaritalStatus'},
+            'gender': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'marital_status_gender'", 'to': u"orm['main.Gender']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'main.position': {
+            'Meta': {'object_name': 'Position'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['main']
